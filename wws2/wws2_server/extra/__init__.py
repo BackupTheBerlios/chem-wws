a2e20e21b063a2fe6e23a158f789dc45f3b1b710
tweimann <tweimann>
2006-09-09 07:22:06 +0000

    extra Verzeichnis verschoben __init__.py zugefügt (extra) license.txt eingefügt (BSD License)

---
+++ wws2/wws2_server/extra/__init__.py
@@ -0,0 +1,12 @@
+# Diese Datei existiert nur, damit sich die Erweiterungen im
+# extra Ordner importieren lassen.
+
+'''
+wws2 - extra
+Regeln:
+Jede Erweiterung muss in einem eigenen Unterverzeichnis liegen,
+eine __init__.py besitzen und ein (oder mehrere) Objekt(e) zur
+Verfügung stellen, die Pyro kompatibel sind.
+'''
+
+__all__ = ['squid']
